0818a9ae22efbf19bcafa5b04dea69586d2a4505
Joanna Wood <caitlinjoannawood@gmail.com>
2019-06-03 21:52:33 +0000

    getRepo function invoked with hardcoded args

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,17 @@
 
 var request = require('request');
 
+
+
+function getRepoContributors(repoOwner, repoName, cb) {
+
 console.log('Welcome to the Github Avatar Downloader!');
+
+}
+
+
+getRepoContributors('jquery', 'jquery', function(err, result) {
+  console.log('Errors: ', err);
+  console.log('Result: ', result);
+});
+
