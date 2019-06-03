1957d003e1f066dfe5f6a64bf4cd57b6c711d1de
Joanna Wood <caitlinjoannawood@gmail.com>
2019-06-03 22:06:51 +0000

    seperate js for access token and set request path to object format

--- download_avatars.js
+++ download_avatars.js
@@ -1,11 +1,21 @@
 
 var request = require('request');
+var token = require('secrets');
 
-
+    //or user-agent can be username based on doc
+var options = {
+  url: 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributers',
+  headers: {
+    'User-Agent':'request'
+  }
+};
 
 function getRepoContributors(repoOwner, repoName, cb) {
 
-console.log('Welcome to the Github Avatar Downloader!');
+  var url = "https://api.github.com/repos/" + repoOwner + '/' + repoName + '/contributers';
+  request(url, function(err, res, body){
+    cb(err, body)
+  });
 
 }
 
