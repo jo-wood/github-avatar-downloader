8226b54d05e9ec4a3a744f7665c3c72ccf0c3432
Joanna Wood <caitlinjoannawood@gmail.com>
2019-06-04 02:45:48 +0000

    fixed getRepo to concat the filepath before sending to downloadImg, exported downloadImg fn, still need to clean up err response and res === 200 instruction

---
+++ download-repoContributors.js
@@ -0,0 +1,43 @@
+var request = require('request');
+var downloadImageByURL = require('./downloadImg.js'); //***** cb fn for getRepoContributors for img download to disk
+
+var secrets = require('./secrets');
+
+var credentials = secrets.GITHUB_TOKEN;
+var repoOwner = 'jquery';
+var repoName = 'jquery';
+
+var options = {
+  url: 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+  headers: {
+    'User-Agent': 'request',
+    'Authorization': credentials
+  }
+};
+
+//***** requests list of contributors from github api
+
+function getRepoContributors(req, cb) {
+  request(req, function(err, res, body){ //! TODO: add better handling for err & nest cb within res === 200 check  
+    cb(err, body); //* the response data is passed to an anonymous callback
+  }); 
+}
+
+
+getRepoContributors(options, function (err, result) {
+  console.log('Errors: ', err);
+  var contributors = JSON.parse(result); //***** the anon cb fn loops through the returned JSON object creating:
+  var avatarArray = []; //***** the urls where the avatar image can be accessed
+  var imagePathArray = []; //*****  array of filepath strings for corresponding users avatar url in avatarArray
+
+  for (var user of contributors) {
+    avatarArray.push(user.avatar_url);
+    imagePathArray.push('./avatars/' + user.login + '.jpg'); //*****  a filepath extension that set a unique filename based on 
+  }
+
+  downloadImageByURL(avatarArray, imagePathArray); //*****  the avatar urls and filepath are then passed to cb fn that dwnlds to disk
+
+}, downloadImageByURL);
+
+
+
--- downloadImg.js
+++ downloadImg.js
@@ -1,13 +1,19 @@
 
 
-var request = require('request');
-var avatarArray = require('./download-avatars')
-avatarArray((arr) => {
-  console.log(arr);
-
-});
 
 
-function downloadImageByUrl(url, filePath) {
+module.exports = function downloadImageByURL(url, filepath) {
+  var fs = require('fs');
 
+//! this loop requires the data to stay in original sequence to link each contributor url
+//! to the relative filepath download
+
+  for (var link of url) {
+    for (var path of filepath) {
+      var wstream = fs.createWriteStream(path);
+      wstream.write(link);
+      wstream.end();
+    }
+  }
+  console.log('Avatars have been successfully downloaded! ');  
 }
